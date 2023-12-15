e32f5ae7d1312481eca9beba7bd6bf00bf48a641
zJvco <65981077+zJvco@users.noreply.github.com>
2023-12-15 20:49:49 -0300

    Adicionando váriaveis de ambiente para desenvolvimento e produção

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL
+
 const api = axios.create({
-    baseURL: "http://localhost:8000/",
+    baseURL: API_URL,
     headers: {
         "Content-Type": "application/json"
     }
