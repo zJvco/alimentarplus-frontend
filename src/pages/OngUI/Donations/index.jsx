f0173a68324f60f857f440d830e938b6c7eed393
zJvco <65981077+zJvco@users.noreply.github.com>
2024-01-18 23:09:03 -0300

    Problema na tabela de doações onde a lógica está errada, alterei para apenas pegar o id da ong/supermercado em vez do nome. Necessário corrigir a logica anterior posteriormente

--- src/pages/OngUI/Donations/index.jsx
+++ src/pages/OngUI/Donations/index.jsx
@@ -13,7 +13,8 @@ import { useEffect } from 'react'
 const muiTableColumns = [
     { field: "id", headerName: "ID da Doação", flex: 1 },
     { field: "id_product", headerName: "ID do Produto", flex: 1 },
-    { field: "supermarket_name", headerName: "Nome do Supermercado", flex: 1 },
+    // { field: "supermarket_name", headerName: "Nome do Supermercado", flex: 1 },
+    { field: "id_supermarket", headerName: "ID do Mercado", flex: 1 },
     { field: "situation", headerName: "Situação", flex: 1 },
     { field: "created_date", headerName: "Data da Operação", flex: 1 },
 ]
@@ -31,48 +32,50 @@ function Donations() {
         return response.data
     }
 
-    const getSupermarketById = async (id) => {
-        const response = await api.get(`/supermarkets/${id}`, {
-            headers: {
-                "Authorization": "Bearer " + token 
-            }
-        })
+    // const getSupermarketById = async (id) => {
+    //     const response = await api.get(`/supermarkets/${id}`, {
+    //         headers: {
+    //             "Authorization": "Bearer " + token 
+    //         }
+    //     })
 
-        return response.data
-    }
+    //     return response.data
+    // }
 
     const getDonationsQuery = useQuery("donations", {
         queryFn: () => getAllDonations()
     })
 
-    const modifiedDonatiosQueries = useQueries(
-        getDonationsQuery.data?.map(donation => {
-            return {
-                queryKey: ['supermarkets', donation.id_supermarket],
-                queryFn: async () => {
-                    const result = await getSupermarketById(donation.id_supermarket)
+    // const modifiedDonatiosQueries = useQueries(
+    //     getDonationsQuery.data?.map(donation => {
+    //         return {
+    //             queryKey: ['supermarkets', donation.id_supermarket],
+    //             queryFn: async () => {
+    //                 const result = await getSupermarketById(donation.id_supermarket)
 
-                    return { ...donation, "supermarket_name": result.name }
-                },
-                // refetchOnWindowFocus: false
-            }
-        }) ?? []
-    )
+    //                 return { ...donation, "supermarket_name": result.name }
+    //             },
+    //             // refetchOnWindowFocus: false
+    //         }
+    //     }) ?? []
+    // )
 
-    const modifiedDonationsIsLoading = modifiedDonatiosQueries.some(result => result.isLoading)
+    // const modifiedDonationsIsLoading = modifiedDonatiosQueries.some(result => result.isLoading)
 
     return (
         <style.Container>
             <h1>Doações</h1>
             <DataGrid
-                rows={!modifiedDonationsIsLoading ? modifiedDonatiosQueries.map(item => item.data) : []}
+                // rows={!modifiedDonationsIsLoading ? modifiedDonatiosQueries.map(item => item.data) : []}
+                rows={getDonationsQuery.data || []}
                 columns={muiTableColumns}
                 checkboxSelection
                 sx={muiCustomDataTableStyle}
                 slots={{
                     toolbar: MUICustomToolBar
                 }}
-                loading={modifiedDonationsIsLoading}
+                // loading={modifiedDonationsIsLoading}
+                loading={getDonationsQuery.isLoading}
                 slotProps={{
                     toolbar: { hasAddNewProductButton: false }
                 }}
